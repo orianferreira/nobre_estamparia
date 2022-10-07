75ff1f15aa4dcee073bd41ed2f2996dc30682b7b
Rian <brianalmeida998@gmail.com>
2022-10-07 17:50:12 -0300

    att

--- src/main/java/com/nobreestamparia/nobreestamparia/repository/ProdutoRepository.java
+++ src/main/java/com/nobreestamparia/nobreestamparia/repository/ProdutoRepository.java
@@ -9,6 +9,6 @@ import com.nobreestamparia.nobreestamparia.model.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
+	
 	public List<Produto> findAllByTituloContainingIgnoreCase(String titulo);
 }
---
+++ src/main/java/com/nobreestamparia/nobreestamparia/security/BasicSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nobreestamparia.nobreestamparia.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class BasicSecurityConfig{
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
